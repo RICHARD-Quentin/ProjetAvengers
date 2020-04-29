838104a3114b1dfc40b799bf8f3508f66329fae3
RICHARD-Quentin <richard.quentin88@gmail.com>
2020-04-29 18:33:12 +0200

    ajout migrations

---
+++ ProjetAvengers/Migrations/20200429080400_CivilId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetAvengers.Migrations
+{
+    public partial class CivilId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CivilsId",
+                table: "Coordonnee",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CivilsId",
+                table: "Coordonnee",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
